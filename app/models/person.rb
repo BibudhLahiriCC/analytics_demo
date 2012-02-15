74520498771f355af5e6861680892915048a6477
Bibudh Lahiri <bibudh@casecommons.org>
2012-02-15 12:28:06 -0500

    Changes in days_since_last_f2f_visit

--- app/models/person.rb
+++ app/models/person.rb
@@ -18,35 +18,25 @@ attr_accessor :created_by_dialog, :generation, :relationship_name, :score
            :class_name => "PhysicalLocation::Placement",
            :extend => CurrentLocationExtension #ensures that placement is current one
 
-  @current_date = Time.now.strftime("%Y-%m-%d")
 
-  scope :in_placement,
-    where('type = ? and date(start_date) <= ? and (date(end_date) is NULL or date(out_of_home_locations.end_date) > ?)',
-          'PhysicalLocation::Placement',
-           '#{current_date}', '#{current_date}')
-  #scope :in_placement_and_had_contact,
-  #      :in_placement.and()
+  def days_since_last_f2f_visit
 
-
-  def last_face_to_face_visit_date
-  #  Person.where()
-  #  Person.all.select{|person| person.placements.any? }
-  #  contact_person.contacts.where(end_date: null)
-  #  placements
-  puts(@current_date)
-  puts(#{self.id})
-  Person.find_by_sql ["select max(date(contacts.occurred_at)) lastVisitDate from people
+   current_date = Time.now.strftime("%Y-%m-%d")
+   puts("#{current_date}")
+   last_visit_date_sql = <<-SQL
+     select '#{current_date}' - max(date(contacts.occurred_at)) lastVisitDate from people
      inner join physical_location_records on physical_location_records.person_id = people.id
-                and physical_location_records.physical_location_type = ?
+                and physical_location_records.physical_location_type = 'PhysicalLocation::Placement'
                 inner join out_of_home_locations on out_of_home_locations.id = physical_location_records.physical_location_id
-                and date(out_of_home_locations.start_date) <= ?
+                and date(out_of_home_locations.start_date) <= '#{current_date}'
                 and (date(out_of_home_locations.end_date) is NULL
-                or date(out_of_home_locations.end_date) > ?)
+                or date(out_of_home_locations.end_date) > '#{current_date}')
      inner join contact_people on contact_people.person_id = people.id
      inner join contacts on contacts.id = contact_people.contact_id
                 and contacts.mode like 'Face to Face%'
-                where people.id = ?",
-                'PhysicalLocation::Placement',
-                '2012-02-14', '2012-02-14', #{self.id}]
+                where people.id = #{self.id}
+   SQL
+
+   last_visit_date = ActiveRecord::Base.connection.select_value(last_visit_date_sql)
   end
 end
